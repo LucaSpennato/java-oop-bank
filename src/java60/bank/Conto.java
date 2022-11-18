36e55cb0f657ad3a0feb82388ff2bfd6efd902be
Luca Spennato <luca.spennato@gmail.com>
2022-11-18 08:43:11 +0100

    add/ vars, get, set, constr, debug in main

--- src/java60/bank/Conto.java
+++ src/java60/bank/Conto.java
@@ -2,4 +2,46 @@ package java60.bank;
 
 public class Conto {
 
+	private int numeroConto;
+	private String nomeProprietario;
+	private int saldo;
+	
+	public Conto(String nomeProprietario) {
+		saldo = 0;
+		this.nomeProprietario = nomeProprietario;
+		numeroConto = 0;
+	}
+
+	public int getNumeroConto() {
+		return numeroConto;
+	}
+
+	public void setNumeroConto(int numeroConto) {
+		this.numeroConto = numeroConto;
+	}
+
+	public String getNomeProprietario() {
+		return nomeProprietario;
+	}
+
+	public void setNomeProprietario(String nomeProprietario) {
+		this.nomeProprietario = nomeProprietario;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Num conto: " + numeroConto
+				+ "\nName: " + nomeProprietario
+				+ "\nSaldo: " + saldo;
+	}
+	
 }
--- src/java60/bank/Main.java
+++ src/java60/bank/Main.java
@@ -2,4 +2,12 @@ package java60.bank;
 
 public class Main {
 
+	public static void main(String[] args) {
+		
+		Conto cn = new Conto("Gianni Fantoni");
+		
+		System.out.println(cn);
+		
+	}
+	
 }
